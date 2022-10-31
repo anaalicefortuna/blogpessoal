305d11dbafc1032ecde130eab38d4f3111e250d7
anaalicefortuna <anaalicefalves@gmail.com>
2022-10-31 10:50:24 -0300

    deploy

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
